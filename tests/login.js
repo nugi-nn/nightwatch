bbc5cca0ffc2246ac065aeca384d32ea1e6a845b
nugi-nn <noviantonugroho@klb.co.id>
2020-05-26 16:21:35 +0700

    rev.

--- tests/login.js
+++ tests/login.js
@@ -10,13 +10,14 @@ module.exports = {
          .assert.urlContains('https://kalcare.karsalintasbuwana.com/', 'Home Page')
          .maximizeWindow();
     browser
-        .useXpath()
-            .assert.elementPresent("//*[@id='__next']/div[1]/header/div/div/button[2]", 'Button Akun')
-            .click("//*[@id='__next']/div[1]/header/div/div/button[2]")
-            .assert.elementPresent("//*[@id='bootstrap-input']")
-            .setValue("//*[@id='bootstrap-input']", email)
-            .click("/html/body/div[2]/div[3]/div/div/div[2]/form/button")
-            .saveScreenshot('tests_output/mkalcare.png')
+    //     .useXpath()
+    //         .assert.elementPresent("//*[@id='__next']/div[1]/header/div/div/button[2]", 'Button Akun')
+    //         .click("//*[@id='__next']/div[1]/header/div/div/button[2]")
+    //         .assert.elementPresent("//*[@id='bootstrap-input']")
+    //         .setValue("//*[@id='bootstrap-input']", email)
+    //         .click("/html/body/div[2]/div[3]/div/div/div[2]/form/button")
+    //         .saveScreenshot('tests_output/mkalcare.png')
+            .pause(5000)
             .end();
  }
 }
